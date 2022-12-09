8b84f5543a1e9be6c5890c456d0a939f3110be80
toan777777 <caoquoctoan7777@gmail.com>
2022-12-09 10:41:15 +0700

    modul2

---
+++ Main.java
@@ -0,0 +1,10 @@
+package QuanLySinhVien;
+
+import QuanLySinhVien.menu.MenuManager;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuManager menuProduct = new MenuManager();
+        menuProduct.menu();
+    }
+}
